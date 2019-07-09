be025c1c0190419019924f7516f49b3b8452cdf8
tbensonwest <tarran@benson-west.com>
2019-07-09 08:36:42 +0100

    final commit

--- app/ch05_jinja_templates/work/pypi_org/app.py
+++ app/ch05_jinja_templates/work/pypi_org/app.py
@@ -1,4 +1,5 @@
 import flask
+from pypi_org.infrastructure.view_modifiers import response
 
 app = flask.Flask(__name__)
 
@@ -10,9 +11,15 @@ def get_latest_packages():
     ]
 
 @app.route('/')
+@response(template_file='home/index.html')
 def index():
     test_packages = get_latest_packages()
-    return flask.render_template('index.html', packages=test_packages)
+    return {'packages': test_packages}
+
+@app.route('/about')
+@response(template_file='home/about.html')
+def about():
+    return {}
 
 if __name__ == '__main__':
     app.run(debug=True)
